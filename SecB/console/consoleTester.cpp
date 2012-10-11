9b307edfe03ccbb9f4e761b0953da7e89986cd86
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2012-10-11 15:17:01 -0400

    console

--- SecB/console/consoleTester.cpp
+++ SecB/console/consoleTester.cpp
@@ -1,32 +1,24 @@
 #include "console.h"
 using namespace cio;
 
+
+
+
 int main(){
-  int ch = 'x';
-  console.setPos(10, 10);
-  console<<"**********************";
-  console.setPos(11, 10);
-  console<<"**********************";
-  console.setPos(12, 10);
-  console<<"**********************";
-  console.pause();
-  console.display("this is a test", 11, 11, 20);
-  console.pause();
-  console.setPos(10, 10);
-  console<<"**********************";
-  console.setPos(11, 10);
-  console<<"**********************";
-  console.setPos(12, 10);
-  console<<"**********************";
-  console.display("this is a testthis is a testthis is a test", 11, 11, 20);
-  console.pause();
-  console.setPos(10, 10);
-  console<<"**********************";
-  console.setPos(11, 10);
-  console<<"**********************";
-  console.setPos(12, 10);
-  console<<"**********************";
-  console.display("this is a testthis is a testthis is a test", 11, 11);
+  char text[100] = "Hello how are you this morning!ABCDEFGHIJKLMNOPQRST";
+  bool insertMode = true;
+  int curpos = 8;
+  int offset = 10;
+  text[31] = 0;
+  console.setPos(10, 20);
+  console<<"*****************";
+  console.setPos(11, 20);
+  console<<"*****************";
+  console.setPos(12, 20);
+  console<<"*****************";
+  console.edit(text, 11, 21, 15, 40, &insertMode, &offset, &curpos, false, false);
+  console.setPos(15,20);
+  console<<text;
   console.pause();
   return 0;
 }
